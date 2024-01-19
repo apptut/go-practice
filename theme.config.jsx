1a81e11a407a4730ccf665f00303c6dd20103cb5
liangqi1 <liangqi1@360.cn>
2024-01-19 19:49:14 +0800

    grpc doc

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,7 +1,24 @@
 export default {
-    logo: <span>Go填坑指南</span>,
+    logo: <span style={{ fontWeight: "500", fontSize: '24px' }}>Go填坑指南</span>,
     project: {
-        link: 'https://github.com/shuding/nextra'
+        link: 'https://github.com/apptut/go-practice'
+    },
+    editLink: {
+      text: null
+    },
+    feedback: {
+        content: null
+    },
+    useNextSeoProps() {
+        return {
+            titleTemplate: '%s – Go 填坑指南'
+        }
+    },
+    sidebar: {
+        toggleButton: true
+    },
+    toc: {
+        title: '大纲',
+        backToTop: true
     }
-    // ... other theme options
 }
